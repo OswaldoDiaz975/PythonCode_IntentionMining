ee8e015da846f8610c4b5db0b76bdfd57d72c13c
OswaldoDiaz975 <76778462+OswaldoDiaz975@users.noreply.github.com>
2022-05-25 16:23:07 -0500

    25/05/22
    Actualzar

--- 9HeuristicSupervisio.py
+++ 9HeuristicSupervisio.py
@@ -2,7 +2,7 @@ import psycopg2
 import itertools
 ## DATABASE CONNECTION AND CURSOR DEFINITION
 # Particular business (sales) strategy log
-conexion = psycopg2.connect(database="postgres", user="postgres", password="espe")
+conexion = psycopg2.connect(database="postgres", user="postgres", password="test")
 cursor=conexion.cursor()
 ## DATA RECUPERATION FROM DATABASE
 cursor.execute("SELECT strategy_rule.strategy_name FROM  public.strategy_rule ORDER BY strategy_num")
@@ -16,29 +16,29 @@ strul = list(itertools.chain.from_iterable(lstrul))
 sql = """INSERT INTO strategy_weight(strategy_name, weight) VALUES(%s,%s)"""
 ## SUPERVISED MACHINE LEARNNING THOUGH THE BUSINESS RULES (HEURISTIC RULES)
 # Verification of supervising rules
-TRADING = ['sale', 'sell', 'seller', 'purchase', 'buy', 'buyer', 'bill', 'billing']
-DEALING = ['order', 'orders' 'quot', 'quotation', 'stock', 'sale', 'sales', 'price', 'customer', 'user', 'users']
+TRADING = ['order', 'quotation', 'stock', 'sale', 'price', 'customer', 'user']
+DEALING = ['seller', 'buyer', 'bill', 'offer', 'promotion', 'billing']
 
 for i in range(len(stname)):
     lst=stname[i].split(' ')
-    # Ponderation by structure of strategy 
+    # Ponderation by structure of strategy (3 points)
     wei1=0
-    if strul[i][0] == 'VERB':
+    if (len(strul[i][0]))==4:
          wei1=1
-    if strul[i][0] == 'NOUN VERB':
+    if (len(strul[i][0]))==9:
         wei1=2
-    if strul[i][0] == 'NOUN VERB NOUN' or strul[i][0] == 'ADJECTIVE NOUN VERB' or strul[i][0] == 'NOUN VERB ADVERB':
+    if (len(strul[i][0]))==55:
         wei1=3 
-    # Verification of TRADING and DEALING supervising rules and ponderation
+    # Verification of TRADING and DEALING supervising rules and ponderation (6 points)
     wei2=0
-    if len([val for val in TRADING if val in lst]) > 0:      
+    if len([val for val in TRADING if val in lst]) > 0:
         strul[i].append("TRADING")
         wei2=wei2+len([val for val in TRADING if val in lst])
     if len([val for val in DEALING if val in lst]) > 0:
         strul[i].append("DEALING")
-        wei2=wei2+len([val for val in TRADING if val in lst])
-    wei=wei1*wei2
-    ponderacion=wei/12 # 12 tokens en total
+        wei2=wei2+len([val for val in DEALING if val in lst])
+    wei=wei1+wei2
+    ponderacion=wei/9
     ## DATABASE UPDATING
     cursor.execute(sql, (stname[i],ponderacion))
     conexion.commit()
